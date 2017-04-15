d7df12a5d18d489cbac6bd68cff3cc171e8096ac
Vinit Anil Gaikwad <vinit.gaikwad10@gmail.com>
2017-04-15 09:19:32 -0700

    Asset Tracking Page

--- app.js
+++ app.js
@@ -13,11 +13,20 @@ app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 
+//app.use(session({
+//	secret : 'Blockchain',
+//	resave : true,
+//	saveUninitialized : false,
+//	cookie : {
+//		secure : true
+//	}
+//}));
+
 app.use(sessions({
-  cookieName: 'session', 
-  secret: 'blockchain', 
-  duration: 24 * 60 * 60 * 1000, 
-  activeDuration: 1000 * 60 * 5
+  cookieName: 'session', // cookie name dictates the key name added to the request object
+  secret: 'blockchain', // should be a large unguessable string
+  duration: 24 * 60 * 60 * 1000, // how long the session will stay valid in ms
+  activeDuration: 1000 * 60 * 5 // if expiresIn < activeDuration, the session will be extended by activeDuration milliseconds
 }));
 
 app.use(app.router);
@@ -30,23 +39,11 @@ if ('development' == app.get('env')) {
 
 app.post('/register', user.register);
 app.post('/login', user.login);
-app.post('/register-product', product.register);
-app.post('/query-product', product.query);
 
+app.post('/register-product', product.register);
 app.post('/registercontract', contract.registercontract);
 app.post('/getcontract', contract.getcontract);
-<<<<<<< Updated upstream
-=======
 app.get('/register-product', product.register);
-app.get('/track/:productId', product.track);
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-
 
 http.createServer(app).listen(app.get('port'), function() {
 	console.log('Express server listening on port ' + app.get('port'));
--- public/javascript/asset-tracking/asset-tracking.js
+++ public/javascript/asset-tracking/asset-tracking.js
@@ -3,19 +3,33 @@
 app.controller('assetTrackingController',['$scope', '$http', function($scope, $http) {
 	$scope.product = {};
 	console.log('loaded');
-	$scope.register = function(){
-		console.log($scope.product);
-		console.log('sending product register post');
-		var data = {
-				productName: $scope.product.name,
-				description: $scope.product.description,
-				category: $scope.product.category,
-				qrCode: $scope.product.id
-            };
 
-        $http.post("/register-product", data).then(function(data, status) {
-           console.log(data);
-        });
+  $scope.states = [];
 
-	};
+  $scope.states[$scope.states.length] = {
+      text:"sdffsfsf"
+
+
+  }
+
+  $scope.states[$scope.states.length] = {
+      text:"Vinit"
+
+
+  }
+	// $scope.register = function(){
+	// 	console.log($scope.product);
+	// 	console.log('sending product register post');
+	// 	var data = {
+	// 			productName: $scope.product.name,
+	// 			description: $scope.product.description,
+	// 			category: $scope.product.category,
+	// 			qrCode: $scope.product.id
+  //           };
+  //
+  //       $http.post("/register-product", data).then(function(data, status) {
+  //          console.log(data);
+  //       });
+  //
+	// };
  }]);
