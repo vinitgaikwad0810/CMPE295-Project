2f29d2639e9c195ac022477a824c269d352b117c
Vinit Anil Gaikwad <vinit.gaikwad10@gmail.com>
2017-04-22 12:10:21 -0700

    Validate Changes

--- routes/blockchain.js
+++ routes/blockchain.js
@@ -1,154 +1,153 @@
 var https = require("https");
 
-var chainCodeName ='deef481fb41ef34ad3fd9c97b70992795d734a20c65b7ec9083677ad7ebc12648736b381e065b1143b2ad1deeb1f892c53d8847ed2d0523402ea325cda149308';
 
-function getProductQueryApiSchema(productId, userId){
-	
-	var queryApiSchema = {
-		     jsonrpc: "2.0",
-		     method: "query",
-		     params: {
-		         type: 1,
-		         chaincodeID: {
-		             name: chainCodeName
-		         },
-		         ctorMsg: {
-		             function: "read",
-		             args: [
-		                 ""+productId
-		             ]
-		         },
-		         secureContext: userId
-		     },
-		     id: 2
-		 };
-	
-	return JSON.stringify(queryApiSchema);
-	
+var chainCodeName = 'f9d4aaf6c78d583c13ce5c411b6658bcb52c68b09f7e9d7ed8fb8f3f8ccec69e3e0df981aeed7eabcf3f7e7ec971c445fba6dff1a4d3f19ced58e6e056f3be7e';
+
+
+
+function getProductQueryApiSchema(productId, userId) {
+
+    var queryApiSchema = {
+        jsonrpc: "2.0",
+        method: "query",
+        params: {
+            type: 1,
+            chaincodeID: {
+                name: chainCodeName
+            },
+            ctorMsg: {
+                function: "read",
+                args: [
+                    "" + productId
+                ]
+            },
+            secureContext: userId
+        },
+        id: 2
+    };
+
+    return JSON.stringify(queryApiSchema);
+
 }
 
 
-function getProductRequestApiSchema(product, user){
-	
-	var productApiRequestSchema = {
-			jsonrpc: "2.0",
-			method: "invoke",
-			params: {
-				type: 1,
-				chaincodeID: {
-					name: chainCodeName
-				},
-				ctorMsg: {
-					function: "register",
-					args: [
-						""+product.productId,
-						JSON.stringify(product)
-						]
-				},
-				secureContext: user
-			},
-			id: 3
-	};
-	
-	return JSON.stringify(productApiRequestSchema); 
+function getProductRequestApiSchema(product, user) {
+
+    var productApiRequestSchema = {
+        jsonrpc: "2.0",
+        method: "invoke",
+        params: {
+            type: 1,
+            chaincodeID: {
+                name: chainCodeName
+            },
+            ctorMsg: {
+                function: "register",
+                args: [
+                    "" + product.productId,
+                    JSON.stringify(product)
+                ]
+            },
+            secureContext: user
+        },
+        id: 3
+    };
+
+    return JSON.stringify(productApiRequestSchema);
 }
 
-exports.registerProduct = function(product, user, peer, callback){
+exports.registerProduct = function(product, user, peer, callback) {
 
-	var target = peer.split(":");
-	var data = getProductRequestApiSchema(product, user);
-	
-	var options = {
-			hostname: target[1].replace("//",""),
-			port: target[2],
-			path: '/chaincode',
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-			}
-	};
+    var target = peer.split(":");
+    var data = getProductRequestApiSchema(product, user);
 
-	var req = https.request(options, function(res) {
-		res.setEncoding('utf8');
-		res.on('data', function (body) {
-			if(body){
-				body = JSON.parse(body);
-				if(body.result && body.result.status === "OK"){
-					callback({
-						status: "success"
-					});
-				}
-			} else {
-				callback({
-					status: "Error on calling blockchain API"
-				});
-			}
+    var options = {
+        hostname: target[1].replace("//", ""),
+        port: target[2],
+        path: '/chaincode',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    };
 
-		});
-	});
+    var req = https.request(options, function(res) {
+        res.setEncoding('utf8');
+        res.on('data', function(body) {
+            if (body) {
+                body = JSON.parse(body);
+                if (body.result && body.result.status === "OK") {
+                    callback({
+                        status: "success"
+                    });
+                }
+            } else {
+                callback({
+                    status: "Error on calling blockchain API"
+                });
+            }
 
-	req.on('error', function(e) {
-		console.log('problem with request: ' + e.message);
-	});
+        });
+    });
 
-	req.write(data);
-	req.end();
+    req.on('error', function(e) {
+        console.log('problem with request: ' + e.message);
+    });
+
+    req.write(data);
+    req.end();
 
 };
 
-exports.queryProduct = function(productId, user, peer, callback){
-	var target = peer.split(":");
-	var data = getProductQueryApiSchema(productId, user);
-	
-	var options = {
-			hostname: target[1].replace("//",""),
-			port: target[2],
-			path: '/chaincode',
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-			}
-	};
+exports.queryProduct = function(productId, user, peer, callback) {
+    var target = peer.split(":");
+    var data = getProductQueryApiSchema(productId, user);
 
-	var req = https.request(options, function(res) {
-		res.setEncoding('utf8');
-		res.on('data', function (body) {
-			if(body){
-				console.log(body);
-				try{
+    var options = {
+        hostname: target[1].replace("//", ""),
+        port: target[2],
+        path: '/chaincode',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    };
+
+    var req = https.request(options, function(res) {
+        res.setEncoding('utf8');
+        res.on('data', function(body) {
+            if (body) {
+                console.log(body);
+                try {
                     body = JSON.parse(body);
-				} catch (err) {
-					console.log('error parsing response body');
+                } catch (err) {
+                    console.log('error parsing response body');
                     callback({
                         status: "error",
                         err: "Error on calling blockchain API"
                     });
-				}
+                }
 
-				if(body.result && body.result.status === "OK"){
-					callback({
-						status: "success",
-						product: JSON.parse(body.result.message)
-					});
-				}
-			} else {
-				callback({
-					status: "error",
-					err: "Error on calling blockchain API"
-				});
-			}
+                if (body.result && body.result.status === "OK") {
+                    callback({
+                        status: "success",
+                        product: JSON.parse(body.result.message)
+                    });
+                }
+            } else {
+                callback({
+                    status: "error",
+                    err: "Error on calling blockchain API"
+                });
+            }
 
-		});
-	});
+        });
+    });
 
-	req.on('error', function(e) {
-		console.log('problem with request: ' + e.message);
-	});
+    req.on('error', function(e) {
+        console.log('problem with request: ' + e.message);
+    });
 
-	req.write(data);
-	req.end();
+    req.write(data);
+    req.end();
 };
-
-
-
-
--- routes/contract.js
+++ routes/contract.js
@@ -1,278 +1,285 @@
 var https = require("https");
 var mongodb = require("./mongo");
 var gitpath = "https://github.com/jagrutipatil/Blockchain_SmartContractEditor";
-var chaincodeName = "46af334915753bfcadfcc0e3ac36e52e22143dcfca866a1c09605a3cf424272e1b4e11e3b0473b56ccc053cadfa19099a7931db108e8580cab984e5e775dd078";
+var chaincodeName = "f9d4aaf6c78d583c13ce5c411b6658bcb52c68b09f7e9d7ed8fb8f3f8ccec69e3e0df981aeed7eabcf3f7e7ec971c445fba6dff1a4d3f19ced58e6e056f3be7e";
 
 var options = {
-		hostname: 'e57848b76d894377a7f176f544757add-vp0.us.blockchain.ibm.com',
-		port: 5001,
-		path: '/chaincode',
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json',
-		}
+    hostname: 'e57848b76d894377a7f176f544757add-vp0.us.blockchain.ibm.com',
+    port: 5001,
+    path: '/chaincode',
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    }
 };
 
 
-function generateInitJSON(functionName, var1, userId){
-	
-	var registrarApiSchema = {
-		     jsonrpc: "2.0",
-		     method: "deploy",
-		     params: {
-		         type: 1,
-		         chaincodeID: {
-		        	 path: gitpath	             
-		         },
-		         ctorMsg: {
-		             function: functionName,
-		             args: [
-		                 var1
-		             ]
-		         },
-		         secureContext: userId
-		     },
-		     id: 4
-		 };
-	
-	return JSON.stringify(registrarApiSchema);	
+function generateInitJSON(functionName, var1, userId) {
+
+    var registrarApiSchema = {
+        jsonrpc: "2.0",
+        method: "deploy",
+        params: {
+            type: 1,
+            chaincodeID: {
+                path: gitpath
+            },
+            ctorMsg: {
+                function: functionName,
+                args: [
+                    var1
+                ]
+            },
+            secureContext: userId
+        },
+        id: 4
+    };
+
+    return JSON.stringify(registrarApiSchema);
 }
 
 exports.validate = function(request, response) {
-	var qrCode = request.body.qrcode;
-	var username = request.body.username;
-	mongodb.queryProduct(qrCode, function (status, id) {
-		if (status != 'error') {	
-			var productid = id;
-			//get peer id from mongodb
-			mongodb.getpeerid(username, function(status, chain_user, peerid) {
-				if (status != "error") {
+    var qrCode = request.body.qrcode;
+    var username = request.body.username;
+    mongodb.queryProduct(qrCode, function(status, id) {
+        if (status != 'error') {
+            var productid = id;
+            //get peer id from mongodb
+            mongodb.getpeerid(username, function(status, chain_user, peerid) {
+                if (status != "error") {
 
-					var params = {
-							lng : request.body.lng,
-							lat : request.body.lat,
-							username : request.body.username, 
-							params : request.body.params 
-					};
+                    var params = {
+                        qrcode: request.body.qrcode,
+                        lng: request.body.lng,
+                        lat: request.body.lat,
+                        username: request.body.username,
+                        params: request.body.params
+                    };
 
-					var eventOptions = {
-							hostname: 'demo2367382.mockable.io',         //PASS PEERID LATER and add port number
-							//port: 5001,
-							path: '/chaincode',
-							method: 'POST',
-							headers: {
-								'Content-Type': 'application/json',
-							}
-					};
-					
-					var data = constructValidateJSON('invoke', chaincodeName, 'validate', params, productid , chain_user);
-					var req = https.request(eventOptions, function(res) {
-						res.setEncoding('utf8');
-						res.on('data', function (body) {
-							if(body){
-								console.log(body);
-								body = JSON.parse(body);
-								if(body.status && body.status === "success"){
-									response.send("status : success");
-						  		} else {
-						  			response.send("status : error");
-						  		}
-							} else {
-								response.send("status : Error on calling blockchain API");
-							}
-						});
-					});
+                    console.log(peerid);
+                    var eventOptions = {
+                        //hostname: 'demo2367382.mockable.io',         //PASS PEERID LATER and add port number
+                        hostname: "e57848b76d894377a7f176f544757add-vp0.us.blockchain.ibm.com",
+                        port: 5001,
+                        path: '/chaincode',
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json',
+                        }
+                    };
 
-					req.on('error', function(e) {
-						response.send('problem with request: ' + e.message);
-					});
+                    var data = constructValidateJSON('invoke', chaincodeName, 'validate', params, productid, chain_user);
+										console.log(data);
+                    var req = https.request(eventOptions, function(res) {
+                        res.setEncoding('utf8');
+                        res.on('data', function(body) {
+                            if (body) {
+                                console.log(body);
+                                body = JSON.parse(body);
+                                if (body.result.status == "OK") {
+                                    response.send("status : success");
+                                } else {
+                                    response.send("status : error");
+                                }
+                            } else {
+                                response.send("status : Error on calling blockchain API");
+                            }
+                        });
+                    });
 
-					req.write(data);
-					req.end();			
-				} else {
-					response.send("status : error");
-				}
-			});
-			
-		} else {
-			response.send("status : error");
-		}
-	});	
+                    req.on('error', function(e) {
+                        response.send('problem with request: ' + e.message);
+                    });
+
+                    req.write(data);
+                    req.end();
+                } else {
+                    response.send("status : error");
+                }
+            });
+
+        } else {
+            response.send("status : error");
+        }
+    });
 };
 
 exports.init = function() {
-	if (chaincodeName === null || chaincodeName === undefined) {
-		var data = generateInitJSON('init', "Hello" , "user_type2_0");
+    if (chaincodeName === null || chaincodeName === undefined) {
+        var data = generateInitJSON('init', "Hello", "user_type2_0");
 
-		var req = https.request(options, function(res) {
-			res.setEncoding('utf8');
-			res.on('data', function (body) {
-				if(body){
-					console.log(body);
-					body = JSON.parse(body);
-					chaincodeName = body.result.message;				
-				} 
-			});
-		});
+        var req = https.request(options, function(res) {
+            res.setEncoding('utf8');
+            res.on('data', function(body) {
+                if (body) {
+                    console.log(body);
+                    body = JSON.parse(body);
+                    chaincodeName = body.result.message;
+                }
+            });
+        });
 
-		req.on('error', function(e) {		
-			console.log(e);
-		});
+        req.on('error', function(e) {
+            console.log(e);
+        });
 
-		req.write(data);
-		req.end();
-	}
+        req.write(data);
+        req.end();
+    }
 }
 
-function constructValidateJSON(methodname, nameparam, functionName, paramsjson, productid , userId){
-	
-	var queryApiSchema = {
-		     jsonrpc: "2.0",
-		     method: methodname,
-		     params: {
-		         type: 1,
-		         chaincodeID: {
-		             name: nameparam
-		         },
-		         ctorMsg: {
-		             function: functionName,
-		             args: [
-		                 paramsjson, productid
-		             ]
-		         },
-		         secureContext: userId
-		     },
-		     id: 4
-		 };
-	
-	return JSON.stringify(queryApiSchema);
-	
+function constructValidateJSON(methodname, nameparam, functionName, paramsjson, productid, userId) {
+
+   var obj = JSON.stringify(paramsjson);
+	 var userId = "user_type2_0";
+	 console.log("obj" + obj)
+    var queryApiSchema = {
+        jsonrpc: "2.0",
+        method: methodname,
+        params: {
+            type: 1,
+            chaincodeID: {
+                name: nameparam
+            },
+            ctorMsg: {
+                function: functionName,
+                args: [
+                    obj, productid
+                ]
+            },
+            secureContext: userId
+        },
+        id: 4
+    };
+
+    return JSON.stringify(queryApiSchema);
+
 }
 
-function constructJSON(methodname, nameparam, functionName, var1, product_type, var2 , userId){
-	
-	var queryApiSchema = {
-		     jsonrpc: "2.0",
-		     method: methodname,
-		     params: {
-		         type: 1,
-		         chaincodeID: {
-		             name: nameparam
-		         },
-		         ctorMsg: {
-		             function: functionName,
-		             args: [
-		                 var1, product_type, var2
-		             ]
-		         },
-		         secureContext: userId
-		     },
-		     id: 4
-		 };
-	
-	return JSON.stringify(queryApiSchema);
-	
+function constructJSON(methodname, nameparam, functionName, var1, product_type, var2, userId) {
+
+    var queryApiSchema = {
+        jsonrpc: "2.0",
+        method: methodname,
+        params: {
+            type: 1,
+            chaincodeID: {
+                name: nameparam
+            },
+            ctorMsg: {
+                function: functionName,
+                args: [
+                    var1, product_type, var2
+                ]
+            },
+            secureContext: userId
+        },
+        id: 4
+    };
+
+    return JSON.stringify(queryApiSchema);
+
 }
 
-function constructGetJSON(methodname, nameparam, functionName, var1,  userId){
-	
-	var queryApiSchema = {
-		     jsonrpc: "2.0",
-		     method: methodname,
-		     params: {
-		         type: 1,
-		         chaincodeID: {
-		             name: nameparam
-		         },
-		         ctorMsg: {
-		             function: functionName,
-		             args: [
-		                 var1
-		             ]
-		         },
-		         secureContext: userId
-		     },
-		     id: 4
-		 };
-	
-	return JSON.stringify(queryApiSchema);
-	
+function constructGetJSON(methodname, nameparam, functionName, var1, userId) {
+
+    var queryApiSchema = {
+        jsonrpc: "2.0",
+        method: methodname,
+        params: {
+            type: 1,
+            chaincodeID: {
+                name: nameparam
+            },
+            ctorMsg: {
+                function: functionName,
+                args: [
+                    var1
+                ]
+            },
+            secureContext: userId
+        },
+        id: 4
+    };
+
+    return JSON.stringify(queryApiSchema);
+
 }
 
-function registercontract (request, response) {
-	if (chaincodeName && chaincodeName != undefined) {
-			var contractid = request.body.contractid;
-			var product_type = request.body.product_type;
-			var contractjson = request.body.params;
-			
-			console.log("Registering the contract for Product Type:" + product_type);
-			console.log("Parameters: " + contractjson);
-			var data = constructJSON('invoke', chaincodeName, 'putcontract', contractid , product_type, contractjson, "user_type2_0");
-			var req = https.request(options, function(res) {
-				res.setEncoding('utf8');
-				res.on('data', function (body) {
-					if(body){
-						console.log(body);
-						body = JSON.parse(body);
-						if(body.result && body.result.status === "OK"){
-							response.send("Success");
-						}
-					} else {
-						response.send("Error on calling blockchain API");
-					}
-				});
-			});
+function registercontract(request, response) {
+    if (chaincodeName && chaincodeName != undefined) {
+        var contractid = request.body.contractid;
+        var product_type = request.body.product_type;
+        var contractjson = request.body.params;
 
-			req.on('error', function(e) {
-				response.send('problem with request: ' + e.message);
-			});
+        console.log("Registering the contract for Product Type:" + product_type);
+        console.log("Parameters: " + contractjson);
+        var data = constructJSON('invoke', chaincodeName, 'putcontract', contractid, product_type, contractjson, "user_type2_0");
+        var req = https.request(options, function(res) {
+            res.setEncoding('utf8');
+            res.on('data', function(body) {
+                if (body) {
+                    console.log(body);
+                    body = JSON.parse(body);
+                    if (body.result && body.result.status === "OK") {
+                        response.send("Success");
+                    }
+                } else {
+                    response.send("Error on calling blockchain API");
+                }
+            });
+        });
 
-			req.write(data);
-			req.end();			
-	} else {		
-		console.log("chaincode not defined yet");
-	}
+        req.on('error', function(e) {
+            response.send('problem with request: ' + e.message);
+        });
+
+        req.write(data);
+        req.end();
+    } else {
+        console.log("chaincode not defined yet");
+    }
 };
 
 exports.getcontract = function(request, response) {
-	
-	var product_type = request.body.product_type;
-	console.log("Getting contract for product_type: " + product_type);
-	console.log(chaincodeName);
-	
-	if (chaincodeName && chaincodeName != undefined) { 
-		var data = constructGetJSON("query", chaincodeName, 'getcontract', "" + product_type, "user_type2_0");
-		
-		var req = https.request(options, function(res) {
-			res.setEncoding('utf8');
-			res.on('data', function (body) {
-				if(body){
-					console.log(body);
-					body = JSON.parse(body);
-					if(body.result && body.result.status === "OK"){					
-						response.send("Success");
-					}
-				} else {
-					response.send("Error on calling blockchain API");
-				}
-			});
-		});
 
-		req.on('error', function(e) {
-			response.send('problem with request: ' + e.message);
-		});
+    var product_type = request.body.product_type;
+    console.log("Getting contract for product_type: " + product_type);
+    console.log(chaincodeName);
 
-		req.write(data);
-		req.end();
-	} else {
-		console.log("No Chaincode ID");
-	}
+    if (chaincodeName && chaincodeName != undefined) {
+        var data = constructGetJSON("query", chaincodeName, 'getcontract', "" + product_type, "user_type2_0");
+
+        var req = https.request(options, function(res) {
+            res.setEncoding('utf8');
+            res.on('data', function(body) {
+                if (body) {
+                    console.log(body);
+                    body = JSON.parse(body);
+                    if (body.result && body.result.status === "OK") {
+                        response.send("Success");
+                    }
+                } else {
+                    response.send("Error on calling blockchain API");
+                }
+            });
+        });
+
+        req.on('error', function(e) {
+            response.send('problem with request: ' + e.message);
+        });
+
+        req.write(data);
+        req.end();
+    } else {
+        console.log("No Chaincode ID");
+    }
 };
 
 exports.modifycontract = function(req, res) {
-	var json = req.body;
-	
-	//call block chain get json API
+    var json = req.body;
+
+    //call block chain get json API
 };
 
 
