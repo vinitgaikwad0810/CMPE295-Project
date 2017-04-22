f24c806cfcf919ef53f5a556c23222cb104fc782
awaise267 <awaise267@gmail.com>
2017-04-21 17:43:52 -0700

    css hacks for qrreader 5

--- public/javascript/register_product/qrreader.js
+++ public/javascript/register_product/qrreader.js
@@ -108,6 +108,7 @@ function read(a)
     //     html+="<a target='_blank' href='"+a+"'>"+a+"</a><br>";
     // html+="<b>"+htmlEntities(a)+"</b><br><br>";
     // document.getElementById("result").innerHTML=html;
+    alert(a);
     parseValues(val);
     $("#myModal").modal("hide");
 }
