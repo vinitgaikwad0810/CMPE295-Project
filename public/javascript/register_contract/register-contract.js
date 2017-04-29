f485ecf81173dee51ca093c6c0884744777acc85
awaise267 <awaise267@gmail.com>
2017-04-29 01:55:59 -0700

    Contract register parameters alignment

--- public/javascript/register_contract/register-contract.js
+++ public/javascript/register_contract/register-contract.js
@@ -1,57 +1,56 @@
-app.controller('registerContractController',['$scope', '$http', function($scope, $http) {
-	$scope.product = {};
-	var counter=0;
-    $scope.questionelemnt = [ {id:counter, key : '', value : '', empty : ''} ];
+app.controller('registerContractController', ['$scope', '$http', function ($scope, $http) {
+    $scope.product = {};
+    var counter = 0;
+    $scope.questionelemnt = [{id: counter, key: '', value: '', empty: ''}];
 
 
-	$scope.registercontract = function(){
-		console.log($scope.product);
-		console.log('sending product register post');
-		var paramsJSON = "{";
-		
-    angular.forEach($scope.questionelemnt, function(order) {
-        paramsJSON += "\"" + order.key + "\" : \"" + order.value + "\", ";
-    });
+    $scope.registercontract = function () {
+        console.log($scope.product);
+        console.log('sending product register post');
+        var paramsJSON = "{";
 
-    paramsJSON += "\"contractName\":\"" + $scope.contract.name + "\" }";
-
-		var data = {
-				params: paramsJSON,
-				contractid: $scope.contract.id,
-				product_type: $scope.contract.category 				
-      };
-		
-        $http.post("/registercontract", data).then(function(data, status) {
-           console.log(data);
-           alert("Contract Created");
-           $scope.contract.id = "";
-           $scope.contract.name = "";
-           $scope.contract.key1 = "";
-           $scope.contract.key2 = "";
-           $scope.contract.key3 = "";
-           $scope.contract.key4 = "";
-           $scope.contract.key5 = "";
-
-           $scope.contract.value1 = "";
-           $scope.contract.value2 = "";
-           $scope.contract.value3 = "";
-           $scope.contract.value4 = "";
-           $scope.contract.value5 = "";
-           $
-          angular.forEach($scope.questionelemnt, function(order) {
-            order.key = "";
-            order.value = "";
-          });
-          $scope.questionelemnt.length = 0;
-          counter = 0;
-          $scope.contract.category = "";
+        angular.forEach($scope.questionelemnt, function (order) {
+            paramsJSON += "\"" + order.key + "\" : \"" + order.value + "\", ";
         });
-		
-	};
 
-	$scope.newItem = function($event){
+        paramsJSON += "\"contractName\":\"" + $scope.contract.name + "\" }";
+
+        var data = {
+            params: paramsJSON,
+            contractid: $scope.contract.id,
+            product_type: $scope.contract.category
+        };
+        $http.post("/registercontract", data).then(function (data, status) {
+            console.log(data);
+            alert("Contract Created");
+            $scope.contract.id = "";
+            $scope.contract.name = "";
+            $scope.contract.key1 = "";
+            $scope.contract.key2 = "";
+            $scope.contract.key3 = "";
+            $scope.contract.key4 = "";
+            $scope.contract.key5 = "";
+
+            $scope.contract.value1 = "";
+            $scope.contract.value2 = "";
+            $scope.contract.value3 = "";
+            $scope.contract.value4 = "";
+            $scope.contract.value5 = "";
+            $
+            angular.forEach($scope.questionelemnt, function (order) {
+                order.key = "";
+                order.value = "";
+            });
+            $scope.questionelemnt.length = 0;
+            counter = 0;
+            $scope.contract.category = "";
+        });
+
+    };
+
+    $scope.newItem = function ($event) {
         counter++;
-        $scope.questionelemnt.push(  { id:counter, key : '', value : '', empty : ''} );
+        $scope.questionelemnt.push({id: counter, key: '', value: '', empty: ''});
         $event.preventDefault();
     }
- }]);
+}]);
