70b40852d4fa2aa857bc0f42c1b6276810117aff
Vinit Anil Gaikwad <vinit.gaikwad10@gmail.com>
2017-05-03 16:42:12 -0700

    Asset Tracking

--- public/javascript/asset-tracking/asset-tracking.js
+++ public/javascript/asset-tracking/asset-tracking.js
@@ -1,6 +1,6 @@
 //var app = angular.module('blockchainApp', ['ngRoute']);
 //AIzaSyAdF4y0AjJujQ248MSKd8KC41wm9fIvpgc
-app.controller('assetTrackingController', ['$scope', '$http', 'NgMap', function ($scope, $http, NgMap, AssetTrackingService) {
+app.controller('assetTrackingController', ['$scope', '$http', 'NgMap', function($scope, $http, NgMap, AssetTrackingService) {
     var vm = $scope;
     $scope.product = {};
     $scope.states = [];
@@ -9,22 +9,22 @@ app.controller('assetTrackingController', ['$scope', '$http', 'NgMap', function 
     $scope.searchErr = "";
 
 
-    $scope.searchButtonClick = function () {
+    $scope.searchButtonClick = function() {
         //  AssetTrackingService.getAssetDetails($scope.searchTerm);
         // Test it with Qr Code 3fdsf-324-234-fdsf
         var url = '/track/' + $scope.searchTerm;
-        $http.get(url).then(function (response) {
+        $http.get(url).then(function(response) {
             console.log(response.data);
             if (response.data.status === "success") {
                 $scope.searchErr = "";
                 $scope.product = response.data.product;
                 $scope.states = $scope.product.states;
-                if($scope.states.length > 0){
-                  $scope.showMap = true;
-                  $scope.setMap();
+                if ($scope.states.length > 0) {
+                    $scope.showMap = true;
+                    $scope.setMap();
                 } else {
-                  $scope.showMap = false;
-                  $scope.searchErr = "Product QRCode has never been scanned after being added to the system";
+                    $scope.showMap = false;
+                    $scope.searchErr = "Product QRCode has never been scanned after being added to the system";
                 }
 
             } else {
@@ -34,17 +34,73 @@ app.controller('assetTrackingController', ['$scope', '$http', 'NgMap', function 
         });
     }
 
-    $scope.setMap = function () {
 
-        NgMap.getMap().then(function (map) {
+    var markers = [];
+    var infoWindows = [];
+    var flightPlanCoordinates = [];
+    var flightPathG;
 
-            var flightPlanCoordinates = [];
-            var markers = [];
-            var infoWindows = [];
+    function setMapOnAll(map) {
+        for (var i = 0; i < markers.length; i++) {
+            markers[i].setMap(map);
+        }
+    }
+
+    // Removes the markers from the map, but keeps them in the array.
+    function clearMarkers() {
+        setMapOnAll(null);
+    }
+
+    // Shows any markers currently in the array.
+    function showMarkers() {
+        setMapOnAll(map);
+    }
+
+    // Deletes all markers in the array by removing references to them.
+    function deleteMarkers() {
+        clearMarkers();
+        markers = [];
+    }
+
+    function setPolylines(flightPlanCoordinates, map) {
+
+
+        var flightPath = new google.maps.Polyline({
+            path: flightPlanCoordinates,
+            geodesic: true,
+            strokeColor: '#FF0000',
+            strokeOpacity: 1.0,
+            strokeWeight: 2
+        });
+
+        flightPath.setMap(map);
+        flightPathG = flightPath;
+
+    }
+
+    function removePolylines() {
+        if (flightPathG !== undefined && flightPathG !== null) {
+            console.log("Flight Path check");
+
+            flightPathG.setMap(null);
+            flightPlanCoordinates = [];
+
+        }
+
+
+    }
+
+    $scope.setMap = function() {
+
+        NgMap.getMap().then(function(map) {
+            clearMarkers();
+            removePolylines();
+            //removeLine();
+
 
             for (var i = 0; i < vm.states.length; i++) {
 
-                var func = function (index) {
+                var func = function(index) {
                     var myLatlng = new google.maps.LatLng(vm.states[index].lat, vm.states[index].lang);
 
                     flightPlanCoordinates[flightPlanCoordinates.length] = myLatlng;
@@ -70,8 +126,8 @@ app.controller('assetTrackingController', ['$scope', '$http', 'NgMap', function 
                     testContent += '</ul>';
 
                     var content = "<strong>STEP " + (index + 1) + "</strong><br>" + testContent;
-                    google.maps.event.addListener(marker, 'click', (function (marker, content, infoWindow) {
-                        return function () {
+                    google.maps.event.addListener(marker, 'click', (function(marker, content, infoWindow) {
+                        return function() {
                             infoWindow.setContent(content);
                             infoWindow.open(map, marker);
                         }
@@ -81,18 +137,21 @@ app.controller('assetTrackingController', ['$scope', '$http', 'NgMap', function 
 
                 func(i);
             }
-            var flightPath = new google.maps.Polyline({
-                path: flightPlanCoordinates,
-                geodesic: true,
-                strokeColor: '#FF0000',
-                strokeOpacity: 1.0,
-                strokeWeight: 2
-            });
+            // var flightPath = new google.maps.Polyline({
+            //     path: flightPlanCoordinates,
+            //     geodesic: true,
+            //     strokeColor: '#FF0000',
+            //     strokeOpacity: 1.0,
+            //     strokeWeight: 2
+            // });
+            //
+            // flightPath.setMap(map);
+            // flightPathG = flightPath;
 
-            flightPath.setMap(map);
+            setPolylines(flightPlanCoordinates, map)
 
             var bounds = new google.maps.LatLngBounds();
-            $.each(markers, function (index, marker) {
+            $.each(markers, function(index, marker) {
                 bounds.extend(marker.position);
             });
             map.fitBounds(bounds);
